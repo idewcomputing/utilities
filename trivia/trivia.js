cc2153fe900a26b589822bb123acd57d53792ed0
Jim Lyst <jlyst@iupui.edu>
2018-04-27 11:52:14 -0400

    Update trivia.js

--- trivia/trivia.js
+++ trivia/trivia.js
@@ -6,56 +6,6 @@ var trivia = {
    categories: [],
    currentQuestion: null,
    currentCategory: null,
-   prepareCategoryButtons: function(buttons) {
-      this.currentCategory = null;
-      this.currentQuestion = null;
-      var $categorySet = $("<div>");
-      buttons.forEach(button => {
-         var $button = $(button);
-         var cat = $button.attr("data-name");
-         if (!this.getUnansweredQuestionFromCategory(cat))
-            $button.attr("disabled", "disabled");
-         $categorySet.append($button);
-         var self = this;
-         $button.one("click", function() {
-            if (typeof onClickedCategoryButton == "function") {
-               self.currentCategory = cat;
-               onClickedCategoryButton(cat);
-            } else alert("Trying to call a function called 'onClickedCategoryButton(category)', but none found!");
-         });
-      });
-      return $categorySet.children();
-   },
-   prepareAnswerButtons: function(q, buttons) {
-      var $thisAnswerSet = $("<div>");
-      this.currentQuestion = q;
-      $thisAnswerSet.html($(buttons));
-      $thisAnswerSet.html(this.getShuffledArray($thisAnswerSet.children()));
-      $thisAnswerSet.one("click", ".answer", function(event) {
-         var isCorrect = $(event.target).hasClass("correct-answer");
-         q.wasCorrectResponse = isCorrect;
-
-         if (typeof onClickedAnswerButton == "function")
-            onClickedAnswerButton(isCorrect);
-         else
-            alert(
-               "Trying to call a function called 'onClickedAnswerButton(question)', but none found!"
-            );
-      });
-      return $thisAnswerSet;
-   },
-   prepareNextButton: function(button) {
-      var $nextButton = $(button);
-      $nextButton.on("click", () => {
-         if (typeof onClickedNextButton == "function")
-            onClickedNextButton(this.currentCategory);
-         else
-            alert(
-               "Trying to call a function called 'onClickedNextButton()', but none found!"
-            );
-      });
-      return $nextButton;
-   },
    loadQuestionBank: function(link) {
       return new Promise((resolve, reject) => {
          Tabletop.init({
@@ -67,12 +17,6 @@ var trivia = {
                this.categories = [
                   ...new Set(this.questions.map(q => q.category))
                ];
-               if (typeof onQuestionBankLoaded == "function")
-                  onQuestionBankLoaded();
-               else
-                  alert(
-                     "Trying to call a function called 'onQuestionBankLoaded()', but none found!"
-                  );
                resolve("success");
             }
          });
